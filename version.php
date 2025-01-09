a7334f01b24b5386cf3db12c1bcf0c4b21845dc5
maysaraadmin <maysara.mubarak.mohamed@gmail.com>
2025-01-10 02:46:51 +0400

    up

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_quranmemorizer';
-$plugin->version = 2024015000; // Increment this number for updates.
+$plugin->version = 2024016000; // Increment this number for updates.
 $plugin->requires = 2021051700; // Moodle 3.11 minimum.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.0';
