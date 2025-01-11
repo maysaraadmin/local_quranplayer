b6b8a3ff89986091d796d851ce813d70b754b692
maysaraadmin <maysara.mubarak.mohamed@gmail.com>
2025-01-12 03:18:33 +0400

    up

--- version.php
+++ version.php
@@ -17,7 +17,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_quranplayer';
-$plugin->version = 2025011003; // YYYYMMDDXX
+$plugin->version = 2025011004; // YYYYMMDDXX
 $plugin->requires = 2020110900; // Minimum Moodle version
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.1';
